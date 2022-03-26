d26ff82f8e337508a9ccfa5396b8198416be9b8b
ALEXXXANDRO <spam0417@yandex.ru>
2022-03-26 15:23:06 +0300

    Chore: init base project

--- Itmo.Dormitory-backend.API/Program.cs
+++ Itmo.Dormitory-backend.API/Program.cs
@@ -1,5 +1,7 @@
+using Itmo.Dormitory_backend.API.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Itmo.Dormitory_backend.API
 {
@@ -12,9 +14,8 @@ namespace Itmo.Dormitory_backend.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .UseSerilog((context, configuration) => configuration.Enrich.FromLogContext().WriteTo.Console())
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .AddInfrastructure();
     }
 }
--- Itmo.Dormitory-backend.API/Startup.cs
+++ Itmo.Dormitory-backend.API/Startup.cs
@@ -1,3 +1,4 @@
+using Itmo.Dormitory_backend.API.Infrastructure.Middlewares;
 using Itmo.Dormitory_backend.Core;
 using Itmo.Dormitory_backend.DataAccess;
 using Microsoft.AspNetCore.Builder;
@@ -5,9 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System.Collections.Generic;
 
 namespace Itmo.Dormitory_backend.API
 {
@@ -23,50 +21,15 @@ namespace Itmo.Dormitory_backend.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DormitoryDbContext>(
-                o => o.UseInMemoryDatabase("SdkDb"));
-
-            services.AddControllers();
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Itmo.Dormitory_backend.API", Version = "v1" });
-
-                options.CustomSchemaIds(selector =>
-                {
-                    var type = selector;
-                    var typeNames = new List<string> { type.Name };
-
-                    while (type?.IsNested ?? false)
-                    {
-                        type = type.DeclaringType;
-                        typeNames.Add(type?.Name);
-                    }
-
-                    typeNames.Reverse();
-                    return string.Join(".", typeNames);
-                });
-            });
+                o => o.UseInMemoryDatabase("DormitoryDb"));
+            services.AddControllers(options => options.Filters.Add(new GlobalExceptionFilter()));
             services.AddCoreModule();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Itmo.Dormitory_backend.API v1"));
-            }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
