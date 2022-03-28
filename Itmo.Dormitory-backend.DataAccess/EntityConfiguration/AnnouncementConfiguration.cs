7100e4696468d6f076cb16527aafb7b46fcf42a9
ALEXXXANDRO <spam0417@yandex.ru>
2022-03-28 16:40:56 +0300

    feat: add announcement domain

---
+++ Itmo.Dormitory-backend.DataAccess/EntityConfiguration/AnnouncementConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Itmo.Dormitory_backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Itmo.Dormitory_backend.DataAccess.EntityConfiguration
+{
+    public class AnnouncementConfiguration : IEntityTypeConfiguration<Announcement>
+    {
+        public void Configure(EntityTypeBuilder<Announcement> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).ValueGeneratedNever();
+
+            var informationConfigurationBuilder = builder.OwnsOne(t => t.Information);
+            informationConfigurationBuilder.Property(pn => pn.Title).HasMaxLength(60);
+            informationConfigurationBuilder.Property(pn => pn.Description).HasMaxLength(2000);
+        }
+    }
+}
