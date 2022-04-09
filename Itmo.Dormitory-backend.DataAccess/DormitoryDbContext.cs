bb1df0a1a967630708a304b6063e2c9ab636026e
ALEXXXANDRO <spam0417@yandex.ru>
2022-04-09 14:18:55 +0300

    feat: use sqlite

--- Itmo.Dormitory-backend.DataAccess/DormitoryDbContext.cs
+++ Itmo.Dormitory-backend.DataAccess/DormitoryDbContext.cs
@@ -4,10 +4,14 @@ namespace Itmo.Dormitory_backend.DataAccess
 {
     public class DormitoryDbContext : DbContext
     {
-        public DormitoryDbContext(DbContextOptions<DormitoryDbContext> options) : base(options) { }
+        public DormitoryDbContext(DbContextOptions<DormitoryDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
         public DbSet<Resident> Residents { get; set; } = null!;
         public DbSet<Announcement> Announcements { get; set; } = null!;
+        public DbSet<Application> Applications { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
