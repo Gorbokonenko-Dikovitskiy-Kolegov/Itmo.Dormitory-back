fdb5c121091600b33e5966a17427def2c9ddf330
ALEXXXANDRO <spam0417@yandex.ru>
2022-04-27 21:40:37 +0300

    chore: add autoMapper, add Sqlite

--- Itmo.Dormitory.Web/Startup.cs
+++ Itmo.Dormitory.Web/Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Itmo.Dormitory.DataAccess;
+using Itmo.Dormitory.Core;
+using Itmo.Dormitory.Core.Announcements;
 
 namespace Itmo.Dormitory.Web
 {
@@ -15,13 +19,15 @@ namespace Itmo.Dormitory.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DormitoryDbContext>(
+                o => o.UseSqlite($"Data Source={@"D:\STUDY\test.db"}")); // I will definitely remove it. Some day.
             services.AddControllersWithViews();
+            services.AddScoped<AnnouncementsAPIController>();
+            services.AddCoreModule();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,7 +37,7 @@ namespace Itmo.Dormitory.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -40,7 +46,6 @@ namespace Itmo.Dormitory.Web
             app.UseRouting();
 
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
