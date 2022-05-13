2364ae0c0281e5c5002b2382f94d1d10de109482
Vitaliy Dikovitskiy <vdikovitsky@nntc.pro>
2022-05-13 16:55:42 +0300

    add CreateUserApi

---
+++ Itmo.Dormitory.Core/Users/Commands/CreateUser.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Itmo.Dormitory.DataAccess;
+using Itmo.Dormitory.Domain.Entities;
+using Itmo.Dormitory.Domain.ValueObjects;
+using JetBrains.Annotations;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Itmo.Dormitory.Core.Users.Commands
+{
+    public static class CreateUser
+    {
+        [PublicAPI]
+        public record Command(
+            string ISUNumber,
+            string Password): IRequest<Response>;
+
+        [UsedImplicitly]
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.ISUNumber).Matches(@"^\d+$");
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        [PublicAPI]
+        public record Response(
+            string Id);
+
+        [UsedImplicitly]
+        public class CommandHandler : IRequestHandler<Command, Response>
+        {
+            private readonly UserManager<IdentityUser> _userManager;
+
+            public CommandHandler(DormitoryDbContext dormitoryDbContext, UserManager<IdentityUser> userManager)
+            {
+                _userManager = userManager;
+            }
+
+            public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = request.ISUNumber,
+                };
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+                return new Response(result.Succeeded ? user.Id : "Fail");
+            }
+        }
+    }
+}
---
+++ Itmo.Dormitory.Core/Users/UsersApiController.cs
@@ -0,0 +1,34 @@
+﻿using Itmo.Dormitory.Core.Users.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itmo.Dormitory.Core.Users
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UsersAPIController
+    {
+        private readonly IMediator _mediator;
+
+        public UsersAPIController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("create-user")]
+        public async Task<ActionResult<CreateUser.Response>> CreateUser(CreateUser.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+        [HttpPost("create-admin")]
+        public async Task<ActionResult<CreateAdmin.Response>> CreateAdmin(CreateAdmin.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
