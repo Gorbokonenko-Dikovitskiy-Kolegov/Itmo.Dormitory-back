e99065bc7dc572905c5460a9b66ede6984a5e702
Vitaliy Dikovitskiy <vdikovitsky@nntc.pro>
2022-05-13 18:02:58 +0300

    add login as admin

--- Itmo.Dormitory.Web/Controllers/AccountController.cs
+++ Itmo.Dormitory.Web/Controllers/AccountController.cs
@@ -20,10 +20,41 @@ namespace Itmo.Dormitory.Controllers
         {
             return View();
         }
-        public IActionResult LoginAsAdmin()
+
+        [HttpGet]
+        public IActionResult LoginAsAdmin(string returnUrl = null)
         {
-            return View();
+            return View(new LoginAsAdminViewModel { ReturnUrl = returnUrl });
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginAsAdmin(LoginAsAdminViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignOutAsync();
+                    // проверяем, принадлежит ли URL приложению
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Announcement");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect login or password");
+                }
+            }
+            return View(model);
+        }
+
 
         [HttpGet]
         public IActionResult LoginAsResident(string returnUrl = null)
