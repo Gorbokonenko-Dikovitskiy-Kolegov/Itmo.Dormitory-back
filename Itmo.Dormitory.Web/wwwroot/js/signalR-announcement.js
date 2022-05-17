20af3bc8bee4b7c6e1e62dc1135f66a9f333f76b
Vitaliy Dikovitskiy <vdikovitsky@nntc.pro>
2022-05-17 16:39:40 +0300

    create announcement creation notification

--- Itmo.Dormitory.Web/wwwroot/js/signalR-announcement.js
+++ Itmo.Dormitory.Web/wwwroot/js/signalR-announcement.js
@@ -3,9 +3,15 @@
     .build();
 
 hubConnection.on('Notify', function (message) {
-
-    alert(message)
-
+    launch_toast()
 });
 
 hubConnection.start();
+
+
+
+function launch_toast() {
+    var x = document.getElementById("toast")
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 5000);
+}
